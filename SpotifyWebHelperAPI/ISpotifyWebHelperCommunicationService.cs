2ed63a21e5cf7972e7d24e336c15b92be38ba328
Joakim Skoog <glasvegas@hotmail.se>
2015-03-23 20:35:28 +0100

    Defined the ISpotifyWebHelperCommunicationService interface which will be main interface that is used by the developer.

---
+++ SpotifyWebHelperAPI/ISpotifyWebHelperCommunicationService.cs
@@ -0,0 +1,41 @@
+﻿using SpotifyWebHelperAPI.Models;
+
+namespace SpotifyWebHelperAPI
+{
+    /// <summary>
+    /// Interface for communicating with the SpotifyWebHelper process that runs locally on user's computers.
+    /// </summary>
+    public interface ISpotifyWebHelperCommunicationService
+    {
+        /// <summary>
+        /// Retrieve the curren status.
+        /// </summary>
+        /// <returns>The current status.</returns>
+        StatusDto GetStatus();
+
+        /// <summary>
+        /// Plays the given URI and returns the updated status.
+        /// </summary>
+        /// <param name="uri">The uri that should be played.</param>
+        /// <returns>The newly updated status.</returns>
+        StatusDto Play(string uri);
+
+        /// <summary>
+        /// Pauses the current song and returns the updated status.
+        /// </summary>
+        /// <returns>The newly updated status.</returns>
+        StatusDto Pause();
+
+        /// <summary>
+        /// Resumes the currently paused song and returns the updated status.
+        /// </summary>
+        /// <returns>The newly updated status.</returns>
+        StatusDto Resume();
+
+        /// <summary>
+        /// Retrieves the client version information.
+        /// </summary>
+        /// <returns>The client version information.</returns>
+        ClientVersionDto GetClientVersion();
+    }
+}
