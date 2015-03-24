12d56085fe17788b051f44bd0410518fd9c363d0
Joakim Skoog <glasvegas@hotmail.se>
2015-03-24 21:06:53 +0100

    First really simple test

--- SpotifyWebHelperAPI.UnitTests/SpotifyWebHelperCommunicationServiceTests.cs
+++ SpotifyWebHelperAPI.UnitTests/SpotifyWebHelperCommunicationServiceTests.cs
@@ -1,4 +1,9 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Rhino.Mocks;
+using Rhino.Mocks.Constraints;
+using SpotifyWebHelperAPI.Models;
+using SpotifyWebHelperAPI.Serialization;
+using SpotifyWebHelperAPI.Web;
 
 namespace SpotifyWebHelperAPI.UnitTests
 {
@@ -6,8 +11,24 @@ namespace SpotifyWebHelperAPI.UnitTests
     public class SpotifyWebHelperCommunicationServiceTests
     {
         [TestMethod]
-        public void TestMethod1()
+        public void GivenThatVersionDataIsReturned_WhenGetStatusIsCalled_ThenTheReturnedStatusDtoContainsTheCorrectVersion()
         {
+            var authProvider = MockRepository.GenerateMock<IAuthProvider>();
+            authProvider.Stub(x => x.GetAuth()).Return("ourAuth");
+            authProvider.Stub(x => x.GetCFID()).Return("ourCfid");
+
+            var webClient = MockRepository.GenerateMock<IWebClient>();
+            webClient.Stub(x => x.DownloadString(Arg<string>.Matches(Text.Contains("ourAuth") && Text.Contains("ourCfid")))).Return("123456");
+
+            var deserializer = MockRepository.GenerateMock<IDeserializer>();
+            deserializer.Stub(x => x.DeserializeObject<StatusDto>(Arg<string>.Is.Equal("123456"))).Return(new StatusDto() { Version = 123456 });
+
+            var service = new SpotifyWebHelperCommunicationService(webClient, new UnixTimeStampConverter(), authProvider, deserializer);
+
+            var status = service.GetStatus();
+
+            Assert.AreEqual(123456, status.Version);
         }
+
     }
 }
