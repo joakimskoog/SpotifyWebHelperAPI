cc6a8f029a2d7127d28352c53b1a06362e02e611
Joakim Skoog <glasvegas@hotmail.se>
2015-04-07 23:18:51 +0200

    Comments and spelling mistakes

--- SpotifyWebHelperAPI/IUnixTimeStampConverter.cs
+++ SpotifyWebHelperAPI/IUnixTimeStampConverter.cs
@@ -3,16 +3,16 @@
 namespace SpotifyWebHelperAPI
 {
     /// <summary>
-    /// Interface for converting from a date to a unix time stamp.
-    /// A unix time stamp is the number of seconds between a given date and the Unix Epoch (1970-01-01 UTC).
+    /// Interface for converting from a date to an unix time stamp.
+    /// An unix time stamp is the number of seconds between a given date and the Unix Epoch (1970-01-01 UTC).
     /// </summary>
     public interface IUnixTimeStampConverter
     {
         /// <summary>
-        /// Converts a DateTime into a unix time stamp.
+        /// Converts a DateTime into an unix time stamp.
         /// </summary>
-        /// <param name="date"></param>
-        /// <returns></returns>
+        /// <param name="date">The DateTime that should be converted into an unix time stamp.</param>
+        /// <returns>The number of seconds between the given date and the Unix Epoch.</returns>
         int ConvertToTimeStamp(DateTime date);
     }
 }
